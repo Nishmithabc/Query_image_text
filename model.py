5aaa8bf05edcaf0dd113e9a4aecfb1be62c194cb
NishmithaBC <horimiyachan1995@gmail.com>
2026-02-16 11:53:02 +0530

    Delete model.py

--- model.py
+++
@@ -1,5 +0,0 @@
-import google.generativeai as genai
-genai.configure(api_key="AIzaSyCxBEcy89KVML7IhybHqInH8j7drLfaYAI")
-
-for m in genai.list_models():
-    print(m.name, m.supported_generation_methods)
